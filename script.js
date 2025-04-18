2656642a422af50715b9bf39b7633d7b505e8f91
marcvlg <marcalvarezruiz9@gmail.com>
2025-04-19 00:14:36 +0200

    header_shadow

--- script.js
+++ script.js
@@ -136,6 +136,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     observer.observe(skillsSection);
   }
+
+  window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY === 0) {
+      header.style.boxShadow = "0 2px 10px rgba(0, 0, 0, 0.1)";
+    } else {
+      header.style.boxShadow = "none";
+    }
+  });
 });
 
 /* ----- SEND WHATSAPP ----- */
